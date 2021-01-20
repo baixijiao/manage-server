bcc4596f691d5b69537663bb145308709bfcdc64
白西郊 <492681225@qq.com>
2021-01-20 11:52:25 +0800

    初始提交

---
+++ server-zyjk/src/main/java/net/htjs/server/zyjk/ZyjkApplication.java
@@ -0,0 +1,13 @@
+package net.htjs.server.zyjk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZyjkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZyjkApplication.class, args);
+    }
+
+}
---
+++ server-zyjk/src/test/java/net/htjs/server/zyjk/ZyjkApplicationTests.java
@@ -0,0 +1,13 @@
+package net.htjs.server.zyjk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZyjkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
