bcae0c76f9f33ad1dcfd3156ac6ebc3e98556d8c
白西郊 <492681225@qq.com>
2021-03-10 17:47:52 +0800

    1.多数据源服务server-common-eureka-dataSources模块，添加ehcache二级缓存。

--- server-common-eureka/server-common-eureka-dataSources/src/main/java/net/htjs/server/dataSources/CommonDataSourcesApplication.java
+++ server-common-eureka/server-common-eureka-dataSources/src/main/java/net/htjs/server/dataSources/CommonDataSourcesApplication.java
@@ -3,6 +3,7 @@ package net.htjs.server.dataSources;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
@@ -10,6 +11,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @EnableEurekaClient
 @EnableDiscoveryClient
 @MapperScan(basePackages = "net.htjs.server.dataSources.ywzx.dao")
+@EnableCaching
 public class CommonDataSourcesApplication {
 
     public static void main(String[] args) {
